632cf25bf2f2144b0c363dbd5ad001043cf739ce
ganupatil4010-crypto <ganupatil4010@gmail.com>
2026-03-19 09:07:47 +0530

    link

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Dynamically set the base URL
 // When deploying, you will replace 'http://localhost:5000' with your live backend URL
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://market-h28s.onrender.com';
 
 const api = axios.create({
   baseURL: BASE_URL,
